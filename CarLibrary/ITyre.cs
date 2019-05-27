0b1ed31e3186616e0f99f870e4898236a962e499
szarlih <39311561+szarlih@users.noreply.github.com>
2019-05-27 16:27:15 +0200

    Add files via upload
    after first excercise

---
+++ CarLibrary/ITyre.cs
@@ -0,0 +1,17 @@
+﻿namespace CarLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public interface ITyre
+    {
+        string Manufacturer { get; }
+
+        string Model { get; }
+
+        int SizeInInch { get; }
+
+        double PressureInBar { get; }
+    }
+}
